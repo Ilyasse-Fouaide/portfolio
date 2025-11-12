a96b218eb055353b0f76282a50cd4c71bc823475
ilyasse-fouaide <ilyasse.fouaide@gmail.com>
2025-11-12 09:51:29 -0500

    remove the ugly b*tch cursor

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,6 @@ import SocialNavBarItems from "./components/SocialNavBarItems";
 function App() {
 	const [nameHover, setNameHover] = React.useState(false);
 
-	const cursorRef = React.useRef();
-
-	const mouseMove = (e) => {
-		const x = e.clientX;
-		const y = e.clientY;
-
-		cursorRef.current.style.top = y + "px";
-		cursorRef.current.style.left = x + "px";
-	};
-
-	useEffect(() => {
-		console.log("load");
-		window.addEventListener("mousemove", mouseMove);
-
-		return () => {
-			window.removeEventListener("mousemove", mouseMove);
-		};
-	}, []);
-
 	React.useEffect(() => {
 		ReactGA.initialize("G-GDMS00V7HH");
 		ReactGA.send({
@@ -40,13 +21,6 @@ function App() {
 	return (
 		<main>
 			<div class="fixed top-0 z-[-2] w-full h-full bg-neutral-950 bg-[radial-gradient(#262626_1px,#0a0a0a_1px)] bg-[size:20px_20px]"></div>
-			{/* cursor */}
-			<div
-				ref={cursorRef}
-				className={`cursor -top-10 text-blue-500 mix-blend-difference z-10 fixed -translate-x-1/2 -translate-y-1/2 bg-neutral-50 rounded-full transition-all duration-200 pointer-events-none ${
-					nameHover ? "w-[200px] h-[200px]" : "w-[80px] h-[80px]"
-				}`}
-			></div>
 			<SocialNavBarItems />
 			<div className="max-w-5xl m-auto">
 				<Info setNameHover={setNameHover} />
